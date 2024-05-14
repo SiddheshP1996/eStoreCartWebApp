dff368e123aec48f1ec0eddb9bf98676bde068ab
SiddheshP1996 <siddheshpanajkar2001@gmail.com>
2024-05-14 23:57:14 +0530

    deployment-changes

--- eCommerceApp/eCommerceApp/settings.py
+++ eCommerceApp/eCommerceApp/settings.py
@@ -83,15 +83,15 @@ WSGI_APPLICATION = 'eCommerceApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -102,7 +102,7 @@ DATABASES = {
         'POST': '3306',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
