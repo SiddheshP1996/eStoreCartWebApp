47b5d04fcae21a14e7677db7f32627eebcc95d16
SiddheshP1996 <siddheshpanajkar2001@gmail.com>
2024-08-12 23:44:52 +0530

    UI-doc-And-JSChange

--- eCommerceApp/static/asset/js/main.js
+++ eCommerceApp/static/asset/js/main.js
@@ -1,34 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // Check for stored theme preference and apply it
-  const storedTheme = localStorage.getItem("theme");
-  if (storedTheme) {
-    document.body.classList.add(storedTheme);
-    const themeIcon = document.getElementById("theme-icon");
-    if (storedTheme === "dark-mode") {
-      themeIcon.classList.remove("bi-sun-fill");
-      themeIcon.classList.add("bi-moon-fill");
-    }
-  }
+  const storedTheme = localStorage.getItem('theme');
+  const themeToggle = document.getElementById('theme-toggle');
+  const themeIcon = document.getElementById('theme-icon');
 
+  if (storedTheme) {
+      document.body.classList.add(storedTheme);
+      if (storedTheme === 'dark-mode') {
+          themeIcon.classList.remove('bi-sun-fill');
+          themeIcon.classList.add('bi-moon-fill');
+          themeToggle.title = "Dark Mode";
+      } else {
+          themeToggle.title = "Light Mode";
+      }
+  } else {
+      themeToggle.title = "Light Mode";
+  }
+  
   // Add event listener to the theme toggle button
-  document
-    .getElementById("theme-toggle")
-    .addEventListener("click", toggleTheme);
+  themeToggle.addEventListener('click', toggleTheme);
 });
 
 function toggleTheme() {
   // Toggle between light and dark mode
-  const isDarkMode = document.body.classList.toggle("dark-mode");
+  const isDarkMode = document.body.classList.toggle('dark-mode');
+  
+  // Update the icon and title based on the theme
+  const themeIcon = document.getElementById('theme-icon');
+  const themeToggle = document.getElementById('theme-toggle');
 
-  // Update the icon based on the theme
-  const themeIcon = document.getElementById("theme-icon");
   if (isDarkMode) {
-    themeIcon.classList.remove("bi-sun-fill");
-    themeIcon.classList.add("bi-moon-fill");
-    localStorage.setItem("theme", "dark-mode");
+      themeIcon.classList.remove('bi-sun-fill');
+      themeIcon.classList.add('bi-moon-fill');
+      themeToggle.title = "Dark Mode";
+      localStorage.setItem('theme', 'dark-mode');
   } else {
-    themeIcon.classList.remove("bi-moon-fill");
-    themeIcon.classList.add("bi-sun-fill");
-    localStorage.setItem("theme", "light-mode");
+      themeIcon.classList.remove('bi-moon-fill');
+      themeIcon.classList.add('bi-sun-fill');
+      themeToggle.title = "Light Mode";
+      localStorage.setItem('theme', 'light-mode');
   }
 }
